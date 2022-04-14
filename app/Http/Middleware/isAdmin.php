86317a8fb9d571787ebf2bb844e96a9148170f41
tuananh19dc <tuananh19dc@gmail.com>
2022-04-14 10:55:20 +0700

    tao middleware phan quyen don gian

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->isAdmin == User::IS_ADMIN) {
+            return $next($request);
+        }
+        
+        return redirect()->back();
+    }
+}
